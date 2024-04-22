d8328d4989de4f48adcd6a4ccda482d52bc9eb54
murtuzzov <murtuzzov>
2024-04-22 23:38:46 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const mainBody = document.querySelector(".main-body");
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+
+button.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  mainBody.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
